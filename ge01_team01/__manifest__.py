1566e1eacd74d10002ac81e5b70a808da064ba78
dizr <dizr@odoo.com>
2023-07-24 17:42:23 -0600

    GE1 finished, filter for detailed_type:'motorcycle' and set as default

---
+++ ge01_team01/__manifest__.py
@@ -0,0 +1,36 @@
+{
+    "name": "Motorcycle Registry",
+    
+    "summary": "Manage Registration of Motorcycles",
+    
+    "description": """
+    Motorcycle Registry
+====================
+This Module is used to keep track of the Motorcycle Reistration and Ownership of each motorcycled of the brand.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["stock"],
+    
+    "data": [
+        "views/product_template_inherit.xml",
+        'views/product_views_inherit.xml',
+    ],
+    
+    "demo": [
+        "demo/motorcycle_registry_demo.xml",
+        "demo/product_demo.xml",
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
