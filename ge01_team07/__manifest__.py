712799558c50e6274782ec2d4021402d0a709004
Carlos Manuel Gonzalez Vallejo <carlosmanuelgonzalezvallejo@MacBook-Air-de-Carlos.local>
2023-07-24 15:35:16 -0600

    GE01 - Base

---
+++ ge01_team07/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':'GE01 Team07',
+    'summary':""" Add filter to products """,
+    'description':""" Kawiil motor TG01
+    ====================
+    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+       """,
+    'license':'OPL-1',
+    'author':'team7',
+    'website':'www.odoo.com',
+    'category':'Kawiil/Admin',
+    'depends':['stock', 'website', 'motorcycle_registry'],
+    'data':[],
+    'demo':[],
+    'application': True,
+}
---
+++ ge01_team07/models/motorcycle_filter.py
@@ -0,0 +1,4 @@
+from odoo import models, fields, api
+
+class Motorcycle_filter(models.Model):
+    _inherit = "product"
