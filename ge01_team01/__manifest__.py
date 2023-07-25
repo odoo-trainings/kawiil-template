af1e0b6be0b08124a69c2c9330330a4fdf2331e8
dizr <dizr@odoo.com>
2023-07-25 10:30:15 -0600

    Correcciones

--- ge01_team01/__manifest__.py
+++ ge01_team01/__manifest__.py
@@ -1,36 +1,24 @@
 {
     "name": "Motorcycle Registry",
-    
     "summary": "Manage Registration of Motorcycles",
-    
     "description": """
     Motorcycle Registry
 ====================
-This Module is used to keep track of the Motorcycle Reistration and Ownership of each motorcycled of the brand.
+    Developer: [DIZR] Diego Zúñiga Rodríguez
+    Task ID: GE1
+    Task URL: https://www.odoo.com/web#id=3426296&cids=17&menu_id=4720&action=4043&model=project.task&view_type=form
+This Module is used to add a filter for products based on 'detailet_type' field, also set it as default in the Sales/Products/Products.
     """,
-    
     "version": "0.1",
-    
-    "category": "Kauil/Registry",
-    
+    "category": "Kauil/Training",
     "license": "OPL-1",
-    
     "depends": ["stock"],
-    
     "data": [
         "views/product_template_inherit.xml",
-        'views/product_views_inherit.xml',
+        "views/product_views_inherit.xml",
     ],
-    
-    "demo": [
-        "demo/motorcycle_registry_demo.xml",
-        "demo/product_demo.xml",
-    ],
-    
-    "author": "kauil-motors",
-    
+    "demo": [],
+    "author": "Odoo Inc",
     "website": "www.odoo.com",
-    
     "application": True,
-    
 }
