99f36ef53e673efe98219a8e0a91d9efa2d55d61
vmac-odoo <vmac@odoo.com>
2023-07-25 10:22:46 -0600

    [ADD] New module GE01

---
+++ ge01_team02/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "GE01: Motorcycle Filter",
+    "summary": "Filter Motorcycle",
+    "description": """
+    Motorcycle GE01_team02""",
+    "version": "0.1",
+    "category": "Kauil/Registry",
+    "license": "OPL-1",
+    "depends": ["motorcycle_registry", "product"],
+    "data": [
+        "views/product_template_search_inherit.xml",
+    ],
+    "author": "kauil-motors",
+    "website": "www.odoo.com",
+    "application": True,
+}
