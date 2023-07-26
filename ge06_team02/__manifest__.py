96b034ceebf753cf2c45672402cb710df7fb2078
vmac-odoo <vmac@odoo.com>
2023-07-26 15:24:25 -0600

    [ADD] GE06 Created

---
+++ ge06_team02/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "GE06: Generate Serial Number as a VIN",
+    "summary": "Generate Serial Number as a VIN",
+    "description": """
+    Motorcycle GE06_team02""",
+    "version": "0.1",
+    "category": "Kauil/Registry",
+    "license": "OPL-1",
+    "depends": ["motorcycle_registry", "stock"],
+    "data": [],
+    "author": "kauil-motors",
+    "website": "www.odoo.com",
+    "application": True,
+}
---
+++ ge06_team02/models/stock_lot.py
@@ -0,0 +1,23 @@
+from odoo import models, fields, api
+
+class StockLot(models.Model):
+    _inherit = 'stock.lot'
+
+    @api.model
+    def _get_next_serial(self, company, product):
+        last_serial = self.env['stock.lot'].search(
+            [('company_id', '=', company.id), ('product_id', '=', product.id)],
+            limit=1, order='id DESC')
+        print(product.product_tmpl_id.detailed_type)
+        if product.product_tmpl_id.detailed_type == 'motorcycle':
+            make = product.make if product.make else 'XX'
+            model = product.model if product.model else 'XX'
+            year = str(product.year) if product.year else '00'
+            battery = product.battery_capacity if product.battery_capacity else 'xl'
+            serial = make + model + year + battery
+            if not last_serial:
+                return '{}{}'.format(serial, '0000001')
+            else:
+                return self.env['stock.lot'].generate_lot_names(last_serial.name, 2)[1]
+        else:
+            super()._get_next_serial(company, product)
