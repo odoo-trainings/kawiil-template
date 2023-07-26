d2aca32ef6bfcba07487e838f06d04ef44d7e095
vmac-odoo <vmac@odoo.com>
2023-07-26 16:53:16 -0600

    [ADD] ge07_team02 module added, only initial files (__init__.py and __manifest__.py)

---
+++ ge07_team02/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "GE7: Motorcyle Registry Manufacturing Enhancements",
+    "summary": "Motorcyle Registry Manufacturing Enhancements",
+    "description": """
+    Motorcycle GE07_team02""",
+    "version": "0.1",
+    "category": "Kauil/Registry",
+    "license": "OPL-1",
+    "depends": ["ge06_team02"],
+    "data": [],
+    "author": "kauil-motors",
+    "website": "www.odoo.com",
+    "application": True,
+}
