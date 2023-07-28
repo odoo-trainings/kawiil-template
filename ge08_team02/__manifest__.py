fcbcbb36b2ba2e129077dff14b82d01f440d1091
vmac-odoo <vmac@odoo.com>
2023-07-28 15:27:41 -0600

    [IMP] Added description and some reformat

--- ge08_team02/__manifest__.py
+++ ge08_team02/__manifest__.py
@@ -2,12 +2,19 @@
     "name": "GE8: Motorcycle Registry Portal",
     "summary": "Motorcycle Registry Portal",
     "description": """
-       
+       Integration to allow users see in portal their motorcycles
+       Features:
+       - List Motorcycle
+       - Search by VIN, Registry Number, License Plate
+       - See Motorcycle Info
     """,
     "version": "1.0",
     "category": "Kauil/Registry",
     "license": "OPL-1",
-    "depends": ["ge07_team02", "portal"],
+    "depends": [
+        "ge07_team02",
+        "portal"
+    ],
     "data": [
         "views/motorcycle_registry_portal_templates.xml"
     ],
