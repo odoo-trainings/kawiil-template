822caf482fe21983621f47c4914bde8ae95fdfe9
“LDLV” <ldlv@odoo.com>
2023-08-01 16:34:19 -0600

    [IMP] GE03 made some corrections so the name does not appear until the required fields are filled so the name and fixed an error that appeared whenever the user tries to create a new product

--- ge03_team2/models/product_template.py
+++ ge03_team2/models/product_template.py
@@ -3,13 +3,13 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    name = fields.Char(compute="_compute_name", store = True)
-
+    name = fields.Char(compute="_compute_name", store = True, default = "")
+    make = fields.Char(default = "")
+    year = fields.Char(default = "")
+    model = fields.Char(default = "")
     @api.depends("year","make","model","detailed_type")
     def _compute_name(self):
         for product in self:
             if product.detailed_type == "motorcycle":
                 product.name = f"{product.year} {product.make} {product.model}"
-            else:
-                self.name = self.name
                 
